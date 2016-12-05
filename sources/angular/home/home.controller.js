62487ed90fd7e3853ecf76cb27cb52b9f79081d6
Darlan Mendonça <darlanmendonca@gmail.com>
2016-12-05 15:20:47 -0200

    add more users to home.controller

--- sources/angular/home/home.controller.js
+++ sources/angular/home/home.controller.js
@@ -12,5 +12,37 @@ function HomeController() {
       name: 'Khaleesi',
       email: 'motherofdragons@targaryen.com',
     },
+    {
+      name: 'Hodor',
+      email: 'hodor@hodor.thedoor',
+    },
+    {
+      name: 'Tyrion Lannister',
+      email: 'theimp@lannisters.com',
+    },
+    {
+      name: 'Cersei Baratheon',
+      email: 'thequeen@lannisters.com',
+    },
+    {
+      name: 'Brandon Stark',
+      email: 'thewarg@starks.com',
+    },
+    {
+      name: 'Robb Stark',
+      email: 'thewolf@starks.com',
+    },
+    {
+      name: 'Jaime Lannister',
+      email: 'thekingslayer@lannisters.com',
+    },
+    {
+      name: 'Sansa Stark',
+      email: 'sonsa@stark.com',
+    },
+    {
+      name: 'Nights King',
+      email: 'nightsking@thewinteriscoming.com',
+    },
   ]
 }
